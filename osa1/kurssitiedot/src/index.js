21ed6a4dcf193df064567b7b2bcf78c77acc0f83
Antti Heimonen <antti.j.t.heimonen@student.jyu.fi>
2019-05-15 13:56:24 +0300

    vaihe 1.3 tehty

--- osa1/kurssitiedot/src/index.js
+++ osa1/kurssitiedot/src/index.js
@@ -9,12 +9,13 @@ const Header = (props) => {
 
 }
 
+
 const Content = (props) => {
     return (
         <div>
-            <Part p={props.p1} e={props.e1} />
-            <Part p={props.p2} e={props.e2} />
-            <Part p={props.p3} e={props.e3} />
+            <Part p={props.p1} />
+            <Part p={props.p2} /> 
+            <Part p={props.p3} /> 
         </div>
     )
 }
@@ -22,33 +23,38 @@ const Content = (props) => {
 
 const Part = (props) => {
     return (
-        <p>{props.p} {props.e} </p>
+        <p>{props.p['name']} {props.p['exercises']} </p>
     )
 }
+
 
 const Total = (props) => {
     return (
-        <p>yhteensä {props.p1 + props.p2 + props.p3 }  tehtävää</p>
+        <p>yhteensä {props.p1 + props.p2 + props.p3 } tehtävää</p>
     )
 }
 
+
 const App = () => {
     const course = 'Half Stack -sovelluskehitys'
-    const part1 = 'Reactin perusteet'
-    const exercises1 = 10
-    const part2 = 'Tiedonvälitys propseilla'
-    const exercises2 = 7
-    const part3 = 'Komponenttien tila'
-    const exercises3 = 14
+    const part1 = {
+        name: 'Reactin perusteet',
+        exercises: 10
+    }
+    const part2 = {
+        name: 'Tiedonvälitys propseilla',
+        exercises: 7
+    }
+    const part3 = {
+        name: 'Komponenttien tila',
+        exercises: 14
+    }
 
     return (
         <div>
             <Header course={course} />
-            <Content p1={part1} e1={exercises1} p2={part2} e2={exercises2} 
-            p3={part3} e3={exercises3} />
-            <Content part={part2} exercises={exercises2} />
-            <Content part={part3} exercises={exercises3} />    
-            <Total p1={exercises1} p2={exercises2} p3={exercises3} />
+            <Content p1={part1} p2={part2} p3={part3} />
+            <Total p1={part1['exercises']} p2={part2['exercises']} p3={part3['exercises']}  />   
         </div>
     )
 }
