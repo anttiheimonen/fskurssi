472ceafbe5b1f79f9fe8e11a0689a27743038d38
Antti Heimonen <anttiheimonen@hotmail.com>
2019-08-26 17:15:58 +0300

    vaihe 2.1 tehty

--- osa2/kurssitiedot/src/index.js
+++ osa2/kurssitiedot/src/index.js
@@ -5,51 +5,66 @@ import ReactDOM from 'react-dom'
 const Header = props =>
   <h1>{props.course}</h1>
 
-const Total = props => {
+/* const Total = props => {
   const total = props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises
 
   return <p>yhteensä {total} tehtävää</p>
-}
+} */
   
 
 const Part = props =>
   <p>{props.part.name} {props.part.exercises}</p>
 
-const Content = props => (
-  <div>
-    <Part part={props.parts[0]} />
-    <Part part={props.parts[1]} />
-    <Part part={props.parts[2]} />
-  </div>
-)
+
+const Content = ({courses}) => {
+    //console.log(courses)
+    return  (
+        <div>
+            {courses.map(course => <Part key={course.id} part={course} /> )}
+        </div>
+    )
+}
+
+
+const Course = ({ course }) => {
+    //console.log(course)
+    
+    return (
+        <div>
+            <Header course={course.name} />
+            <Content courses={course.parts} />
+        </div>
+    )
+}
 
 const App = () => {
-  const course = {
-    name: 'Half Stack -sovelluskehitys',
-    parts: [
-      {
-        name: 'Reactin perusteet',
-        exercises: 10
-      },
-      {
-        name: 'Tiedonvälitys propseilla',
-        exercises: 7
-      },
-      {
-        name: 'Komponenttien tila',
-        exercises: 14
-      }
-    ]
-  } 
-
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  )
-}
+    const course = {
+      name: 'Half Stack application development',
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        }
+      ]
+    }
+  
+    return (
+      <div>
+        <Course course={course} />
+      </div>
+    )
+  }
 
 ReactDOM.render(
   <App />,
