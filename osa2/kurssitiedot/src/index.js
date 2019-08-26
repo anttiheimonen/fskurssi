9f78bb9ff4905126aac26a9c26d9dff394323d97
Antti Heimonen <anttiheimonen@hotmail.com>
2019-08-26 20:04:04 +0300

    vaihe 2.4 tehty

--- osa2/kurssitiedot/src/index.js
+++ osa2/kurssitiedot/src/index.js
@@ -3,16 +3,17 @@ import ReactDOM from 'react-dom'
 
 // Pohjana tehtävälle käytetty kurssin mallivastausta 1-5
 
-const Header = props =>
-  <h1>{props.course}</h1>
+const CourseHeader = props =>
+  <h2>{props.course}</h2>
 
 
 const Total = ({parts}) => {
     const sumFunction = (sum, cur) => sum + cur.exercises
     // Reduce-funktion alkuarvon (0) asettaminen estää tyhjän taulukon
     // muutoin aiheuttamat virheilmoitukset
-    const total = parts.reduce(sumFunction, 0)    
-    return <p>total of {total} excercises</p>
+    const total = parts.reduce(sumFunction, 0)   
+    // Lihavaitu <b> tagilla, mutta olisiko css oikea ratkaisu? 
+    return <b>total of {total} excercises</b>
 }
   
 
@@ -21,7 +22,6 @@ const Part = props =>
 
 
 const Content = ({courses}) => {
-    //console.log(courses)
     return  (
         <div>
             {courses.map(course => <Part key={course.id} part={course} /> )}
@@ -33,41 +33,81 @@ const Content = ({courses}) => {
 const Course = ({ course }) => {
     return (
         <div>
-            <Header course={course.name} />
+            <CourseHeader course={course.name} />
             <Content courses={course.parts} />
             <Total parts={course.parts} />
         </div>
     )
 }
 
+
+const Courses = ({ courses }) => {
+  // Kurssien tiedoissa ei ole saatavilla hyvää luontevaa
+  // key-arvoa, joten käytetään kurssin nimeä avaimina.
+  return (
+      <div>
+          {courses.map(course => <Course key={course.name} course={course} />)}
+      </div>
+  )
+}
+
+
+const PageHeader = ({header}) => <h1>{header}</h1>
+
+
 const App = () => {
-    const course = {
-      name: 'Half Stack application development',
-      parts: [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10,
-          id: 1
-        },
-        {
-          name: 'Using props to pass data',
-          exercises: 7,
-          id: 2
-        },
-        {
-          name: 'State of a component',
-          exercises: 14,
-          id: 3
-        }
-      ]
-    }
+    const courses = [
+      {
+        name: 'Half Stack application development',
+        parts: [
+          {
+            name: 'Fundamentals of React',
+            exercises: 10,
+            id: 1
+          },
+          {
+            name: 'Using props to pass data',
+            exercises: 7,
+            id: 2
+          },
+          {
+            name: 'State of a component',
+            exercises: 14,
+            id: 3
+          },
+          {
+            name: 'Redux',
+            exercises: 11,
+            id: 4
+          }
+        ]
+      }, 
+      {
+        name: 'Node.js',
+        parts: [
+          {
+            name: 'Routing',
+            exercises: 3,
+            id: 1
+          },
+          {
+            name: 'Middlewares',
+            exercises: 7,
+            id: 2
+          }
+        ]
+      }
+    ]
+
+    const mainHeader = 'Web development curriculum'
   
     return (
       <div>
-        <Course course={course} />
+        <PageHeader header={mainHeader}/>
+        <Courses courses={courses} />
       </div>
     )
-  }
+}
 
 ReactDOM.render(
   <App />,
