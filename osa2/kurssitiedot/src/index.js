0a0370f0e2183ef37a666dc01ec993723c33c7be
Antti Heimonen <anttiheimonen@hotmail.com>
2019-08-28 22:20:10 +0300

    vaihe 2.5 tehty, korjattu Courses-moduulin import

--- osa2/kurssitiedot/src/index.js
+++ osa2/kurssitiedot/src/index.js
@@ -1,57 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './components/Courses'
+import Courses from './components/Courses'
 
 // Pohjana tehtävälle käytetty kurssin mallivastausta 1-5
-
-const CourseHeader = props =>
-  <h2>{props.course}</h2>
-
-
-const Total = ({parts}) => {
-    const sumFunction = (sum, cur) => sum + cur.exercises
-    // Reduce-funktion alkuarvon (0) asettaminen estää tyhjän taulukon
-    // muutoin aiheuttamat virheilmoitukset
-    const total = parts.reduce(sumFunction, 0)   
-    // Lihavaitu <b> tagilla, mutta olisiko css oikea ratkaisu? 
-    return <b>total of {total} excercises</b>
-}
-  
-
-const Part = props =>
-  <p>{props.part.name} {props.part.exercises}</p>
-
-
-const Content = ({courses}) => {
-    return  (
-        <div>
-            {courses.map(course => <Part key={course.id} part={course} /> )}
-        </div>
-    )
-}
-
-
-const Course = ({ course }) => {
-    return (
-        <div>
-            <CourseHeader course={course.name} />
-            <Content courses={course.parts} />
-            <Total parts={course.parts} />
-        </div>
-    )
-}
-
-
-const Courses = ({ courses }) => {
-  // Kurssien tiedoissa ei ole saatavilla hyvää luontevaa
-  // key-arvoa, joten käytetään kurssin nimeä avaimina.
-  return (
-      <div>
-          {courses.map(course => <Course key={course.name} course={course} />)}
-      </div>
-  )
-}
-
 
 const PageHeader = ({header}) => <h1>{header}</h1>
 
