cf485873f4246626df47369800b25d79a8fd1076
Antti Heimonen <anttiheimonen@hotmail.com>
2019-09-01 13:47:56 +0300

    vaihe 2.11 tehty. Axios ja Effect Hookit lisätty

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,18 +1,21 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
 import PersonForm from './components/PersonForm'
 import Numbers from './components/Numbers'
 import FilterField from './components/FilterField'
 
 const App = () => {
-  const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [ persons, setPersons] = useState([])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber] = useState('')
   const [ filterText, setFilterText ] = useState('')
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons').then(response => {
+      console.log(response.data)
+      setPersons(response.data)
+    })
+  }, [])
 
   const handleNameChange = (event) =>
     setNewName(event.target.value)
@@ -22,7 +25,6 @@ const App = () => {
 
   const handleFilterChange = (event) =>
     setFilterText(event.target.value)
-
 
   const addEntry = (event) => {
     event.preventDefault()
