09eb65b49a59c3e69f31e326bd2108ed9c53f270
Antti Heimonen <anttiheimonen@hotmail.com>
2019-10-29 18:03:04 +0200

    Blogin otsikko tunnistaa klikkauksen ja tuotaa konsoliviestin

--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,27 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState, useImperativeHandle } from 'react'
+
+// const Blog = React.forwardRef((props, ref) => {
+const Blog = React.forwardRef((props, ref) => {
+  const [visible, setVisible] = useState(false)
+
+  const hideWhenVisible = { display: visible ? 'none' : '' }
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  useImperativeHandle(ref, () => {
+    return {
+      toggleVisibility
+    }
+  })
+
+  return (
+    <div onClick={toggleVisibility}>>
+      {props.blog.title} {props.blog.author}
+    </div>
+  )
+})
 
 export default Blog
