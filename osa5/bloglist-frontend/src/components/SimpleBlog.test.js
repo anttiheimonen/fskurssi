58627fbe92406dea48ead740f36a51e68071fbc6
Antti Heimonen <anttiheimonen@hotmail.com>
2019-11-18 18:52:18 +0200

    Siirretty oletus blogin määrittäminen lohkon alkuun

--- osa5/bloglist-frontend/src/components/SimpleBlog.test.js
+++ osa5/bloglist-frontend/src/components/SimpleBlog.test.js
@@ -3,46 +3,44 @@ import { render, fireEvent } from '@testing-library/react'
 import { prettyDOM } from '@testing-library/dom'
 import SimpleBlog from './SimpleBlog'
 
-test('renders content', () => {
+
+describe('SimpleBlog', () => {
   const blog = {
     author: 'Aku Ankka',
     title: 'Living in duckburg',
     likes: '4'
   }
 
-  const onClick = () => console.log('testing')
+  test('renders content', () => {
+    const onClick = () => console.log('testing')
 
-  const component = render(
-    <SimpleBlog blog={blog} onClick={onClick} />
-  )
+    const component = render(
+      <SimpleBlog blog={blog} onClick={onClick} />
+    )
 
-  const header = component.container.querySelector('.header')
-  expect(header).toHaveTextContent(
-    'Living in duckburg Aku Ankka'
-  )
+    const header = component.container.querySelector('.header')
+    expect(header).toHaveTextContent(
+      'Living in duckburg Aku Ankka'
+    )
 
-  const content = component.container.querySelector('.content')
-  expect(content).toHaveTextContent(
-    'blog has 4 likes'
-  )
-})
+    const content = component.container.querySelector('.content')
+    expect(content).toHaveTextContent(
+      'blog has 4 likes'
+    )
+  })
 
 
-test('clicking like button twice fire two events', async () => {
-  const blog = {
-    author: 'Aku Ankka',
-    title: 'Living in duckburg',
-    likes: '4'
-  }
+  test('clicking like button twice fire two events', async () => {
+    const mockHandler = jest.fn()
 
-  const mockHandler = jest.fn()
+    const { getByText } = render(
+      <SimpleBlog blog={blog} onClick={mockHandler} />
+    )
 
-  const { getByText } = render(
-    <SimpleBlog blog={blog} onClick={mockHandler} />
-  )
+    const button = getByText('like')
+    fireEvent.click(button)
+    fireEvent.click(button)
+    expect(mockHandler.mock.calls.length).toBe(2)
+  })
 
-  const button = getByText('like')
-  fireEvent.click(button)
-  fireEvent.click(button)
-  expect(mockHandler.mock.calls.length).toBe(2)
 })
